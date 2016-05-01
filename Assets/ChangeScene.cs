04d33ca67fb3a12e552c3b04ab4c2dab973f50c5
shifalijain <shifali.j21@gmail.com>
2016-05-01 19:49:10 -0400

    final code

---
+++ Assets/ChangeScene.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeScene : MonoBehaviour {
+
+	
+	public void changeToScene (string sceneID) {
+
+        Application.LoadLevel(sceneID);
+        
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,131 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using System.Timers;
+
+public class PlayerController : MonoBehaviour {
+    private Rigidbody rb;
+    public float speed;
+    public Text winText;
+    private int count;
+    public AudioSource[] sounds;
+    public Text countText;
+    //public AudioSource noise1;
+    //public AudioSource noise2;
+    public DeactivatePath path1;
+    void Start()
+    {
+        
+        rb = GetComponent<Rigidbody>();
+        
+        count = 0;
+        if(Application.loadedLevelName=="oldGame")
+        countText.text = "COUNT: " + count.ToString();
+        winText.text = "";        
+    }	
+
+    void FixedUpdate()
+    {
+
+         Vector3 m_Movement = new Vector3(Input.acceleration.x, 0, Input.acceleration.y);
+         transform.Translate(m_Movement * 10 * Time.deltaTime);
+
+        
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+            Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+            rb.AddForce(movement * speed);
+
+        }
+
+    void OnTriggerEnter(Collider other)
+    {
+        
+
+        if (other.gameObject.CompareTag("Pick up"))
+        {
+
+            PlaySounds(0);
+            other.gameObject.SetActive(false);
+            count = count + 1;
+
+            if(Application.loadedLevelName == "FollowThePath" && count==1)
+            {
+                
+                Application.LoadLevel("GreatJob");
+
+               
+                
+            }
+
+            if(Application.loadedLevelName == "Sample2" && count==5)
+            {
+
+                //YouWin();
+                Application.LoadLevel("miniGame");
+
+            }
+
+            if (Application.loadedLevelName == "oldGame")
+            {
+
+                countText.text = "COUNT: " + count.ToString();
+                if(count == 10)
+                {
+                    YouWin();
+
+                    Application.LoadLevel("miniGame");
+                    return;
+                }
+            }
+            // string str = "YOU WIN..!";
+            if (count==14 && Application.loadedLevelName == "miniGame")
+            {
+                
+
+                    YouWin();
+                    Application.LoadLevel("Level 3");
+                    return;
+                
+
+            }
+
+            if (Application.loadedLevelName == "Level 3" && count == 14)
+            {
+                GameObject go = GameObject.Find("Path1Right");
+                go.SetActive(false);
+            }
+
+
+            if (count == 20 && Application.loadedLevelName == "Level 3")
+            {
+                YouWin();
+            }
+        }
+    }
+
+    void YouWin()
+    {
+        PlaySounds(1);
+        winText.text = "YOU WIN...!";
+    }
+    void PlaySounds(int index)
+    {
+        sounds = GetComponents<AudioSource>();
+        sounds[index].Play();
+       
+    }
+
+   
+    void setCount()
+    {
+        count = count + 1;
+
+    }
+  
+}
+
+
